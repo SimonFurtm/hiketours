0690bb33b6d8a5a8fe5939da58bec4cb411b1167
SimonFurtm <82949946+SimonFurtm@users.noreply.github.com>
2022-11-25 13:18:39 +0100

    Integrate Map

--- client/screens/main/HomeScreen.tsx
+++ client/screens/main/HomeScreen.tsx
@@ -3,12 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/organism/EditScreenInfo';
 import { Text, View } from '../../components/atoms/Themed';
 import { RootTabScreenProps } from '../../types';
+import Map from '../../components/organism/Map';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Map />
     </View>
   );
 }
