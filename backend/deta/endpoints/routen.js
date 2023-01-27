93a34aacf42895b8cc19da5ab5b2563c98c62c97
LuxAlexander <65600622+LuxAlexander@users.noreply.github.com>
2023-01-27 17:01:19 +0100

    sss

--- backend/deta/endpoints/routen.js
+++ backend/deta/endpoints/routen.js
@@ -14,7 +14,7 @@ const ObjectId = require("mongodb").ObjectId;
 // This section will help you get a list of all the Routs.
 Routes.route("/api/allroutes").get(function (req, res) {
   let db_connect = dbo.getDb();
-  console.log()
+  console.log(db_connect);
   db_connect
     .collection("Routen")
     .find({})
